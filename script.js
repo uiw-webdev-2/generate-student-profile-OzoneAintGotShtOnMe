a2f78c338611f904bfcc879d95239ce19233cd4e
OzoneAintGotShtOnMe <59840042+OzoneAintGotShtOnMe@users.noreply.github.com>
2022-03-04 12:54:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const studentProfile = {
+    firstName: "Noah",
+    lastName: "Torrey",
+    email: "torrey@student.uiwtx.edu",
+    pidm: "54321",
+    school:"University of the Incarnate Word",
+    gradeLevel:"Senior",
+    avatar:"image.jpg",
+    ImageAlt: "my beautiful, well defined face complete with so many features"
+  };
+  
+const content = `
+      <h1>${studentProfile.firstName} ${studentProfile.lastName}</h1>
+      <ul>
+        <li>Email address:<span> ${studentProfile.email}</span></li>
+        <li>PIDM:<span> ${studentProfile.pidm}</span></li>
+        <li>School: <span> ${studentProfile.school}</span></li>
+        <li>Current grade level:<span> ${studentProfile.gradeLevel}</span></li>
+      </ul>  
+  `;
 
+const createImage = (dataObj) => {
+    let newImage = document.createElement("pic");
+    newImage.setAttribute("src", dataObj.avatar);
+    newImage.setAttribute("alt", dataObj.imageAlt);
+    return newImage;
+}
+
+const populate = (profile) => {//
+    let newArticle = document.createElement("article");
+    newArticle.innerHTML = content;
+    newArticle.prepend(createImage(studentProfile));
+    return newArticle;
+}
+
+
+
+document.querySelector("main").append(populate(profile));
